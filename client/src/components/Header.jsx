ba2513d18d5700b385579297e99012318703f2d5
Navodya <it22608772@my.sliit.lk>
2025-02-08 09:42:32 +0530

    change application name in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         
       <Link to='/'>
       <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className='text-slate-500'>Navodya</span>
+        <span className='text-slate-500'>Real</span>
         <span className='text-slate-700'>Estate</span>
       </h1>
       </Link>
